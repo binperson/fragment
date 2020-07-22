fe191a95b15630f8ecd503f3e76a04709d0a6420
binperson <binperson@binpersondeMacBook-Pro.local>
2020-07-22 23:38:29 +0800

    feat:add

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import LikeButtonTwo from './components/LikeButtonTwo'
 import LikeButtonThree from './components/LikeButtonThree'
 import MouseTracker from './components/MouseTracker'
 
+import useMousePosition from './hooks/useMousePosition'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [ show, setSow ] = useState(true)
+  const positions = useMousePosition()
   return (
     <div className="App">
       <header className="App-header">
+        <p>X: {positions.x}, Y : {positions.y}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
+
+import useMousePosition from '../hooks/useMousePosition'
+
 const LikeButton: React.FC = () => {
 	const [like, setLike] = useState(0)
+	const positions = useMousePosition()
 	useEffect(() => {
 		document.title = 'a' + like
 	}, [like])
 	return (
 		<button onClick={() => {setLike(like + 1)}}>
 			{ like }👍
+			<p>X: {positions.x}, Y : {positions.y}</p>
 		</button>
 	)
 }
