2bb2ae8ef4286d0f627c68aa02646d4a3be14111
Xu Bin <549346275@qq.com>
2020-09-26 17:00:01 +0800

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -1,3 +1,28 @@
+type Point = { x: number, y: number }
+
+function tsDemo(data: Point) {
+  return Math.sqrt(data.x ** 2 + data.y ** 2)
+}
+
+tsDemo({
+  x: 1,
+  y: 2
+})
+
+//
+
+interface Point { x: number, y: number }
+
+function tsDemo(data: Point) {
+  return Math.sqrt(data.x ** 2 + data.y ** 2)
+}
+
+tsDemo({
+  x: 1,
+  y: 2
+})
+
+
 interface Person {
   readonly id: number;
   name: string;
