98d8d7843bc2376cd95c1ca5fb810bb55746b9d8
Xu Bin <549346275@qq.com>
2020-09-27 13:29:28 +0800

    Update basic-types.ts

--- basic-types.ts
+++ basic-types.ts
@@ -28,4 +28,34 @@ function test() {
   console.log(arguments)
 }
 
+// 数组
+const arr: (number | string)[] = [1, '2', 3]
+
 let user: [string, number] = ['viking', 1]
+
+// type alias 类型别名
+type User = {name: string, age: number}
+
+const objectArr: User[] = [{
+  name: 'dell',
+  age: 28
+}]
+
+class Teacher {name: string; age: number}
+
+const objectArr: Teacher[] = [
+  new Teacher(),
+  {
+    name: 'dell',
+    age: 28
+  }
+]
+
+// 元组 tuple
+const teacherInfo: [string, string, number] = ['Dell', 'male', 18]
+// csv
+const teacherList: [string, string, number][] = [
+  ['Dell', 'male', 18],
+  ['Dell1', 'male', 18],
+  ['Dell2', 'male', 18]
+]
